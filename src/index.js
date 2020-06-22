382896df9ffe224c9ae7f7263d45e9a6e49b962b
xiaoyufei <915724526@qq.com>
2020-06-22 17:03:23 +0800

    feat: 优化循环逻辑

--- src/index.js
+++ src/index.js
@@ -10,29 +10,27 @@ export default function (babel) {
                     const {file} = path.hub
                     const {node} = path
                     // 获取参数
-                    const opts = state.opts || {}
+                    const {opts} = state
                     const globalStrings = opts.globalString || []
                     if (globalStrings.length === 0) return
-                    for (let string of globalStrings) {
-                        // 是以属性调用的形式使用 string.xxx
-                        if (!t.isIdentifier(path.node.object, {name: string})) continue
-                        let result = ''
-                        const property = node.property
-                        // 是否自定义了 string 变量
-                        if (file.scope.getBinding(string) || !property) continue
-                        let preParent = null
-                        let parent = path
-                        // 要由大写字母开头
-                        while (parent && t.isIdentifier(parent.node.property) && /^[A-Z].*/.test(parent.node.property.name)) {
-                            result += `${parent.node.property.name}-`
-                            preParent = parent
-                            parent = parent.findParent((temp) => temp.isMemberExpression())
-                        }
-                        if (preParent) {
-                            preParent.replaceWith(t.stringLiteral(result.slice(0, -1)))
-                        }
+                    // 找到是以属性调用的形式使用 string.xxx
+                    const string = globalStrings.find(e => t.isIdentifier(path.node.object, {name: e}))
+                    if (!string) return
+                    let result = ''
+                    const property = node.property
+                    // 是否自定义了 string 变量
+                    if (file.scope.getBinding(string) || !property) return
+                    let preParent = null
+                    let parent = path
+                    // 要由大写字母开头
+                    while (parent && t.isIdentifier(parent.node.property) && /^[A-Z].*/.test(parent.node.property.name)) {
+                        result += `${parent.node.property.name}-`
+                        preParent = parent
+                        parent = parent.findParent((temp) => temp.isMemberExpression())
                     }
-
+                    if (preParent) {
+                        preParent.replaceWith(t.stringLiteral(result.slice(0, -1)))
+                    }
                 },
             },
         }
